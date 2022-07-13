5fe391bca9168d89b732b2be49de87bbe6dbe9d4
Aidan LeMay <ahl4753@rit.edu>
2022-07-13 00:44:38 -0500

    Adding /status Route for Order Status Queries

---
+++ routes/status.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { Orders } = require('../model/model');
+require('dotenv').config(); //initialize dotenv
+
+router.get('/', async (req, res) => {
+    const order = await Orders.findOne({"_id": req.body.id});
+    res.status(200).json({"orderID": order._id, "dasherAssigned": order.dasherAssigned, "acceptTime": order.acceptTime, "orderComplete": order.orderComplete});
+});
+
+module.exports = router;
