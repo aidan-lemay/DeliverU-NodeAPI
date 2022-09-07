cc77eaa8c1dfc4c5e05c084f9fbdf2e6ebabc304
Aidan LeMay <aidanpathways12@gmail.com>
2022-09-06 23:21:12 -0400

    Updating Model for String Delivery of Phone Number

--- model/model.js
+++ model/model.js
@@ -39,7 +39,7 @@ const orderSchema = new mongoose.Schema({
     },
     customerPhone: {
         required: false,
-        type: Number,
+        type: String,
         default: null
     },
     customerInstructions: {
