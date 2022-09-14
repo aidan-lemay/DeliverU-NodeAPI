eb47447ffb905b9e382a7086693c803913d41de3
Aidan LeMay <aidanpathways12@gmail.com>
2022-09-14 16:02:33 -0400

    Updating No Runner Configuration, Ready for Testing

--- routes/order.js
+++ routes/order.js
@@ -30,18 +30,26 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 // TEST Server
 const channels = {
     2760: { // RIT
+        "dash-role": "994984065465847878", // ID of @Dashers role
+        "dispatch-channel": "1013260097532727316", // ID of PUBLIC #new-orders
         "order-logging": "1013260148883591298", // ID of PRIVATE #order-logging
         "control-channel": "1013260202616823978", // ID of PRIVATE #bot-control
     },
     5816: { // UNC
+        "dash-role": "994984065465847878", // ID of @Dashers role
+        "dispatch-channel": "1013260280328880249", // ID of PUBLIC #new-orders
         "order-logging": "1013260306308403301", // ID of PRIVATE #order-logging
         "control-channel": "1013260332556361738", // ID of PRIVATE #bot-control
     },
     2928: { // UB
+        "dash-role": "994984065465847878", // ID of @Dashers role
+        "dispatch-channel": "1017411461351481344", // ID of PUBLIC #new-orders
         "order-logging": "1017411483803582545", // ID of PRIVATE #order-logging
         "control-channel": "1017411509485305919", // ID of PRIVATE #bot-control
     },
     2925: { // U of R
+        "dash-role": "994984065465847878", // ID of @Dashers role
+        "dispatch-channel": "1017411560374800444", // ID of PUBLIC #new-orders
         "order-logging": "1017411585469337610", // ID of PRIVATE #order-logging
         "control-channel": "1017411607556530267", // ID of PRIVATE #bot-control
     }
@@ -72,15 +80,29 @@ async function runDispatch(id, locationCode) {
 
 }
 
+async function runNotify(locationCode) {
+    let dispatch = channels[locationCode]['dispatch-channel']
+
+    const Channel = client.channels.cache.get(dispatch);
+    if (!Channel) return console.log("Invalid channel.");
+    Channel.send('<@&' + channels[locationCode]['dash-role'] + '> Orders are waiting! Please clock-in to accept!');
+}
+
 router.post('/', async (req, res) => {
     const id = req.body.id;
     const updatedData = req.body.data;
+    let clockedRunners = 0;
 
-    // ERROR CHECKING!!!!!
     const order = await Orders.findOne({ _id: ObjectId(id) });
-    const runners = await Clocked.count({ clockedIn: true });
 
-    if (runners > 0) {
+    for await (const doc of Runners.find({ user_locationcode: order.locationCode })) {
+        const runner = await Clocked.find({ user_id: doc.user_id });
+        if (runner.clockedIn) {
+            clockedRunners ++;
+        }
+    }
+
+    if (clockedRunners > 0) {
         if (order == undefined) {
             res.status(500).json({ error: "Supplied ID was not found in the database" });
         }
@@ -97,6 +119,7 @@ router.post('/', async (req, res) => {
         }
     }
     else {
+        runNotify(order.locationCode);
         res.status(503).json({ "orderAccepted": false, error: "No Runners Currently Available, Please Try Again In A Few Minutes - Runners Have Been Notified Of Your Order" });
     }
 
